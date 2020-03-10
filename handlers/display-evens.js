1c4ac339e606efd3f24da11b9917739025cce771
mesut <gelukkig.zijn.be@gmail.com>
2020-03-10 13:41:35 +0100

    display evens

--- handlers/display-evens.js
+++ handlers/display-evens.js
@@ -1,6 +1,17 @@
 function evensHandler() {
   debugger;
-  // write me!
+  // update state, this will be used after..important!
+  strings.displayed = 'evens';
+
+  // re-render UI from state
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
+
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+
+  const listContainer = document.getElementById('selected');
+  listContainer.innerHTML = '';
+  listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
